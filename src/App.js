b4f7ab370aa6868a5f965dbb09afdb63104f7f29
mandara-fernando <mandarajfx@gmail.com>
2021-09-16 20:32:51 +0530

    Updated Initial js files (App.js,Index.js)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import AllNav from "./components/AllNav";
+import React, { useEffect, useState } from "react";
 
+import { BrowserRouter as Router } from "react-router-dom";
+axios.defaults.withCredentials = true;
+//Main function
 function App() {
+  // Sprinner
+  const [isLoading, setLoading] = useState(true);
+  function fakeRequest() {
+    return new Promise((resolve) => setTimeout(() => resolve(), 800));
+  }
+
+  useEffect(() => {
+    fakeRequest().then(() => {
+      const el = document.querySelector(".loader-container");
+      if (el) {
+        el.remove();
+        setLoading(!isLoading);
+      }
+    });
+  }, []);
+
+  if (isLoading) {
+    return null;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <AllNav />
+      </Router>
     </div>
   );
 }
