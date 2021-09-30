f8a85b88c4c7227b8bbe74375d7007b72e9ae211
mandara-fernando <mandarajfx@gmail.com>
2021-10-01 00:29:10 +0530

    Address book sub components added with relevant styles

---
+++ src/components/Customer/Profile/Profile Sub Components/AddressBook/AddAddress.js
@@ -0,0 +1,78 @@
+import React from "react";
+import { Card, Col, Form, Row } from "react-bootstrap";
+import { Button, Typography } from "@material-ui/core";
+import Select from "react-select";
+
+function AddAddress(props) {
+  const options = [
+    { value: "Colombo", label: "Colombo" },
+    { value: "Galle", label: "Galle" },
+    { value: "Kaluthara", label: "Kaluthara" },
+    { value: "Kurunegala", label: "Kurunegala" },
+  ];
+
+  return (
+    <div>
+      <Card className={"p-5 mb-3"}>
+        {" "}
+        <div className="text-center mb-2">
+          <h1 className="form-titles ">Add New Address</h1>
+          <hr className="divide" />
+        </div>
+        <Form.Group as={Col} controlId="formGridAddress">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Ex: My Home"
+            // onChange={(e) => {
+            //     setAddress(e.target.value);
+            // }}
+          />
+        </Form.Group>
+        <Form.Group as={Col} controlId="formGridAddress">
+          <Form.Label>No</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Ex:102/3"
+            // onChange={(e) => {
+            //     setAddress(e.target.value);
+            // }}
+          />
+        </Form.Group>
+        <div className={"mb-4"}>
+          <Form.Group className={"mb-4"}>
+            <Form.Label>City</Form.Label>
+            <Select
+              maxMenuHeight={125}
+              // value={selectedOption}
+              // onChange={selectSize}
+              options={options}
+            />
+          </Form.Group>
+        </div>
+        <div className="d-flex  gap-2">
+          <Button
+            className={"cancel-button"}
+            variant="outlined"
+            onClick={() => {
+              props.close("add");
+            }}
+          >
+            Cancel
+          </Button>
+          <Button
+            className={"save-button"}
+            variant="outlined"
+            // onClick={() => {
+            //   props.fun("address");
+            // }}
+          >
+            Save
+          </Button>
+        </div>
+      </Card>
+    </div>
+  );
+}
+
+export default AddAddress;
