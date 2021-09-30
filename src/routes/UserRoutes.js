8e7059830cbf2bf3d3608e643d09a100129c7fbc
mandara-fernando <mandarajfx@gmail.com>
2021-10-01 00:24:13 +0530

    User Routes Updated!

--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -1,26 +1,19 @@
-
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import Home from "../components/Customer/Main Pages/Home";
-// import Payment from "../components/Customer/Checkout/Payment";
-import login from "../components/Authentications/Login";
-import SignUp from "../components/Authentications/SignUp";
 import Menu from "../components/Customer/Menu";
 import Cart from "../components/Customer/Main Pages/Cart";
+import Home from "../components/Customer/Main Pages/Home";
+import Profile from "../components/Customer/Profile/Profile";
 function UserRoutes() {
   return (
     <>
       <Route path="/home" component={Home} />
 
       <Route path="/menu" component={Menu} />
-        
+
       <Route path="/cart" component={Cart} />
-        
-      {/*<Route path="/payment" component={Payment} />*/}
-        
-      <Route path="/login" component={login} />
-        
-      <Route path="/signUp" component={SignUp} />
+
+      <Route path="/customer/profile" component={Profile} />
     </>
   );
 }
